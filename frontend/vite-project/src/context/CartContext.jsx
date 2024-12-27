c12a60adf2e2f036183ef363300546daac125e84
Pinob Hanee <ponza961@gmail.com>
2024-12-27 15:03:44 +0200

    pre-final

--- frontend/vite-project/src/context/CartContext.jsx
+++ frontend/vite-project/src/context/CartContext.jsx
@@ -1,4 +1,4 @@
-/* eslint-disable react-hooks/exhaustive-deps */
+/* eslint-disable react-refresh/only-export-components */
 import { createContext, useContext, useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 
